d8c044d42aff55119e3818c71f2cb71a05cad72e
Kerovazzky <rednoll12@gmail.com>
2020-11-11 01:35:57 +0300

    optimize FileResource

--- src/main/java/com/kero/security/ksdl/resource/FileResource.java
+++ src/main/java/com/kero/security/ksdl/resource/FileResource.java
@@ -8,7 +8,7 @@ public abstract class FileResource<T> implements KsdlWritableResource<T> {
 
 	protected Path path;
 	protected Path repositoryPath;
-
+	
 	public FileResource(Path repositoryPath, Path path) {
 
 		this.repositoryPath = repositoryPath;
@@ -20,12 +20,10 @@ public abstract class FileResource<T> implements KsdlWritableResource<T> {
 		
 		String fileSeparator = this.path.getFileSystem().getSeparator();
 		
-		String repositoryPath = this.repositoryPath.toAbsolutePath().toString();
-		String rawPath = this.path.toAbsolutePath().toString();
-			rawPath = rawPath.substring(repositoryPath.length());
-			rawPath = rawPath.replaceFirst("\\..+$", "");
+		String addressPath = this.repositoryPath.relativize(this.path).toString();
+			addressPath = addressPath.replaceFirst("\\..+$", "");
 			
-		String address = rawPath.replaceAll("\\"+fileSeparator, "\\"+ResourceAddress.SEPARATOR);
+		String address = addressPath.replaceAll("\\"+fileSeparator, "\\"+ResourceAddress.SEPARATOR);
 		
 		if(address.startsWith(".")) {
 		
